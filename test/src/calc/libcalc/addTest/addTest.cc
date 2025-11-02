d9beb4ee66af75334bc86fa44ad1dd2a434375ff
Tetsuo Honda <t-honda@hondarer-soft.com>
2025-11-02 16:45:50 +0900

    構造整理

---
+++ test/src/calc/libcalc/addTest/addTest.cc
@@ -0,0 +1,39 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <test_com.h>
+#include <mock_stdio.h>
+
+#include <libcalc.h>
+
+using namespace testing;
+
+class addTest : public Test
+{
+};
+
+TEST_F(addTest, test_1_add_2)
+{
+    // Arrange
+
+    // Pre-Assert
+
+    // Act
+    int rtc = add(1, 2); // [手順] - add(1, 2) を呼び出す。
+
+    // Assert
+    EXPECT_EQ(3, rtc); // [確認] - 戻り値が 3 であること。
+}
+
+TEST_F(addTest, test_2_add_1)
+{
+    // Arrange
+
+    // Pre-Assert
+
+    // Act
+    int rtc = add(2, 1); // [手順] - add(2, 1) を呼び出す。
+
+    // Assert
+    EXPECT_EQ(3, rtc); // [確認] - 戻り値が 3 であること。
+}
