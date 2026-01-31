724481a9f394c4ff00551a30261ae5e792059293
Hondarer <Hondarer@users.noreply.github.com>
2026-01-31 04:08:22 +0000

    deploy: 443359ab96d58fdf9d8f37e56183ab0d46691f79

---
+++ doxygen/calc.net/search/pages_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['wrapper_0',['c-modernization-kit サンプル (calc dotnet wrapper)',['../index.html',1,'']]]
+];
---
+++ doxygen/calc/search/defines_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['winapi_0',['WINAPI',['../libcalc_8h.html#a9aa60e1ead64be77ad551e745cbfd4d3',1,'libcalc.h']]]
+];
