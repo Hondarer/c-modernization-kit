7ac24bed795323864b77625fcb650b4593c9b57c
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-02-06 07:35:37 +0900

    プラットフォーム検出系マクロの定義

---
+++ prod/include/compiler.h
@@ -0,0 +1,96 @@
+/**
+ *******************************************************************************
+ *  @file           compiler.h
+ *  @brief          コンパイラ検出および抽象化マクロのヘッダーファイル。
+ *  @author         c-modernization-kit sample team
+ *  @date           2026/02/06
+ *
+ *  コンパイラの種類とバージョンを検出し、統一的なマクロを定義します。\n
+ *  また、コンパイラ固有のインライン制御属性を抽象化します。
+ *
+ *  @copyright      Copyright (C) CompanyName, Ltd. 2025. All rights reserved.
+ *
+ *******************************************************************************
+ */
+
+#ifndef COMPILER_H
+#define COMPILER_H
+
+/**
+ *  @name           コンパイラ検出マクロ
+ *  @brief          コンパイラの種類・名称・バージョンを定義します。
+ *
+ *  @details
+ *  検出されたコンパイラに応じて、以下のマクロを定義します。
+ *
+ *  | コンパイラ | 識別マクロ       | COMPILER_NAME | COMPILER_VERSION の形式             |
+ *  | ---------- | ---------------- | ------------- | ----------------------------------- |
+ *  | MSVC       | COMPILER_MSVC    | "MSVC"        | _MSC_VER の値 (例: 1943)            |
+ *  | Clang      | COMPILER_CLANG   | "Clang"       | major * 10000 + minor * 100 + patch |
+ *  | GCC        | COMPILER_GCC     | "GCC"         | major * 10000 + minor * 100 + patch |
+ *  | その他     | COMPILER_UNKNOWN | "Unknown"     | 0                                   |
+ *
+ *  @note           Clang は __GNUC__ も定義するため、Clang を GCC より先に判定しています。
+ *  @{
+ */
+#ifdef DOXYGEN
+    #define COMPILER_MSVC            /*!< MSVC コンパイラの場合に定義されます。 */
+    #define COMPILER_CLANG           /*!< Clang コンパイラの場合に定義されます。 */
+    #define COMPILER_GCC             /*!< GCC コンパイラの場合に定義されます。 */
+    #define COMPILER_UNKNOWN         /*!< 未知のコンパイラの場合に定義されます。 */
+    #define COMPILER_NAME  "name"    /*!< コンパイラ名の文字列です ("MSVC", "Clang", "GCC", "Unknown")。 */
+    #define COMPILER_VERSION 0       /*!< コンパイラバージョンの数値です。 */
+#else
+    #if defined(_MSC_VER)
+        #define COMPILER_MSVC
+        #define COMPILER_NAME    "MSVC"
+        #define COMPILER_VERSION _MSC_VER
+    #elif defined(__clang__)
+        #define COMPILER_CLANG
+        #define COMPILER_NAME    "Clang"
+        #define COMPILER_VERSION (__clang_major__ * 10000 + __clang_minor__ * 100 + __clang_patchlevel__)
+    #elif defined(__GNUC__)
+        #define COMPILER_GCC
+        #define COMPILER_NAME    "GCC"
+        #define COMPILER_VERSION (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+    #else
+        #define COMPILER_UNKNOWN
+        #define COMPILER_NAME    "Unknown"
+        #define COMPILER_VERSION 0
+    #endif
+#endif
+/** @} */
+
+/**
+ *  @name           インライン制御マクロ
+ *  @brief          コンパイラ固有のインライン制御属性を抽象化します。
+ *
+ *  @details
+ *  コンパイラごとに異なるインライン強制・抑制の構文を、
+ *  統一的なマクロで提供します。
+ *
+ *  | マクロ名     | MSVC                 | GCC/Clang                             | その他 |
+ *  | ------------ | -------------------- | ------------------------------------- | ------ |
+ *  | FORCE_INLINE | __forceinline        | inline __attribute__((always_inline)) | inline |
+ *  | NO_INLINE    | __declspec(noinline) | __attribute__((noinline))             | (空)   |
+ *
+ *  @{
+ */
+#ifdef DOXYGEN
+    #define FORCE_INLINE             /*!< インライン展開を強制します。コンパイラに応じた属性に展開されます。 */
+    #define NO_INLINE                /*!< インライン展開を抑制します。コンパイラに応じた属性に展開されます。 */
+#else
+    #if defined(COMPILER_MSVC)
+        #define FORCE_INLINE __forceinline
+        #define NO_INLINE    __declspec(noinline)
+    #elif defined(COMPILER_GCC) || defined(COMPILER_CLANG)
+        #define FORCE_INLINE inline __attribute__((always_inline))
+        #define NO_INLINE    __attribute__((noinline))
+    #else
+        #define FORCE_INLINE inline
+        #define NO_INLINE
+    #endif
+#endif
+/** @} */
+
+#endif /* COMPILER_H */
---
+++ prod/include/platform.h
@@ -0,0 +1,113 @@
+/**
+ *******************************************************************************
+ *  @file           platform.h
+ *  @brief          プラットフォームおよびアーキテクチャ検出マクロのヘッダーファイル。
+ *  @author         c-modernization-kit sample team
+ *  @date           2025/11/22
+ *
+ *  ビルド対象の OS とプロセッサアーキテクチャを検出し、統一的なマクロを定義します。
+ *
+ *  @copyright      Copyright (C) CompanyName, Ltd. 2025. All rights reserved.
+ *
+ *******************************************************************************
+ */
+
+#ifndef PLATFORM_H
+#define PLATFORM_H
+
+#include "compiler.h"
+
+/**
+ *  @name           プラットフォーム検出マクロ
+ *  @brief          ビルド対象の OS を検出し、識別マクロと名称文字列を定義します。
+ *
+ *  @details
+ *  検出されたプラットフォームに応じて、以下のマクロを定義します。
+ *
+ *  | プラットフォーム | 識別マクロ           | PLATFORM_NAME       |
+ *  | ---------------- | -------------------- | ------------------- |
+ *  | Windows          | PLATFORM_WINDOWS     | "Windows"           |
+ *  | Linux            | PLATFORM_LINUX       | "Linux"             |
+ *  | macOS            | PLATFORM_MACOS       | "macOS"             |
+ *  | Apple (非 macOS) | PLATFORM_APPLE_OTHER | "Apple (non-macOS)" |
+ *  | その他           | PLATFORM_UNKNOWN     | "Unknown"           |
+ *
+ *  @note           Apple プラットフォームでは TargetConditionals.h を使用して
+ *                  macOS と iOS 等を区別しています。
+ *  @{
+ */
+#ifdef DOXYGEN
+    #define PLATFORM_WINDOWS         /*!< Windows の場合に定義されます。 */
+    #define PLATFORM_LINUX           /*!< Linux の場合に定義されます。 */
+    #define PLATFORM_MACOS           /*!< macOS の場合に定義されます。 */
+    #define PLATFORM_APPLE_OTHER     /*!< macOS 以外の Apple プラットフォームの場合に定義されます。 */
+    #define PLATFORM_UNKNOWN         /*!< 未知のプラットフォームの場合に定義されます。 */
+    #define PLATFORM_NAME "name"     /*!< プラットフォーム名の文字列です ("Windows", "Linux", "macOS", "Apple (non-macOS)", "Unknown")。 */
+#else
+    #if defined(_WIN32) || defined(_WIN64)
+        #define PLATFORM_WINDOWS
+        #define PLATFORM_NAME "Windows"
+    #elif defined(__linux__)
+        #define PLATFORM_LINUX
+        #define PLATFORM_NAME "Linux"
+    #elif defined(__APPLE__)
+        #include <TargetConditionals.h>
+        #if TARGET_OS_MAC && !TARGET_OS_IPHONE
+            #define PLATFORM_MACOS
+            #define PLATFORM_NAME "macOS"
+        #else
+            #define PLATFORM_APPLE_OTHER
+            #define PLATFORM_NAME "Apple (non-macOS)"
+        #endif
+    #else
+        #define PLATFORM_UNKNOWN
+        #define PLATFORM_NAME "Unknown"
+    #endif
+#endif
+/** @} */
+
+/**
+ *  @name           アーキテクチャ検出マクロ
+ *  @brief          ビルド対象のプロセッサアーキテクチャを検出し、識別マクロと名称文字列を定義します。
+ *
+ *  @details
+ *  検出されたアーキテクチャに応じて、以下のマクロを定義します。
+ *
+ *  | アーキテクチャ | 識別マクロ   | ARCH_NAME |
+ *  | -------------- | ------------ | --------- |
+ *  | x86_64         | ARCH_X64     | "x64"     |
+ *  | x86 (32bit)    | ARCH_X86     | "x86"     |
+ *  | AArch64        | ARCH_ARM64   | "ARM64"   |
+ *  | ARM (32bit)    | ARCH_ARM     | "ARM"     |
+ *  | その他         | ARCH_UNKNOWN | "Unknown" |
+ *
+ *  @{
+ */
+#ifdef DOXYGEN
+    #define ARCH_X64                 /*!< x86_64 アーキテクチャの場合に定義されます。 */
+    #define ARCH_X86                 /*!< x86 (32bit) アーキテクチャの場合に定義されます。 */
+    #define ARCH_ARM64               /*!< AArch64 アーキテクチャの場合に定義されます。 */
+    #define ARCH_ARM                 /*!< ARM (32bit) アーキテクチャの場合に定義されます。 */
+    #define ARCH_UNKNOWN             /*!< 未知のアーキテクチャの場合に定義されます。 */
+    #define ARCH_NAME "name"         /*!< アーキテクチャ名の文字列です ("x64", "x86", "ARM64", "ARM", "Unknown")。 */
+#else
+    #if defined(__x86_64__) || defined(_M_X64)
+        #define ARCH_X64
+        #define ARCH_NAME "x64"
+    #elif defined(__i386__) || defined(_M_IX86)
+        #define ARCH_X86
+        #define ARCH_NAME "x86"
+    #elif defined(__aarch64__) || defined(_M_ARM64)
+        #define ARCH_ARM64
+        #define ARCH_NAME "ARM64"
+    #elif defined(__arm__) || defined(_M_ARM)
+        #define ARCH_ARM
+        #define ARCH_NAME "ARM"
+    #else
+        #define ARCH_UNKNOWN
+        #define ARCH_NAME "Unknown"
+    #endif
+#endif
+/** @} */
+
+#endif /* PLATFORM_H */
