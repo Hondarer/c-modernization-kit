d0f536fc482fe1b3b65db0c0f12819470ad383c7
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-02-18 23:41:53 +0900

    警告オプションの調整

---
+++ makepart.mk
@@ -0,0 +1,24 @@
+GCC_WARN_BASE = \
+	-Wall -Wextra \
+	-Wformat=2 \
+	-Wshadow -Wundef \
+	-Wpointer-arith -Wcast-qual -Wcast-align \
+	-Wswitch-enum -Wswitch-default \
+	-Wpacked -Wpadded \
+	-Wunknown-pragmas
+GCC_WARN_C_ONLY = \
+	-Wmissing-prototypes \
+	-Wstrict-prototypes \
+	-Wmissing-declarations
+
+ifneq ($(OS),Windows_NT)
+    # Linux
+    CFLAGS      = $(GCC_WARN_BASE) $(GCC_WARN_C_ONLY)
+    CXXFLAGS    = $(GCC_WARN_BASE)
+    LDFLAGS     =
+else
+    # Windows
+    CFLAGS      =
+    CXXFLAGS    =
+    LDFLAGS     =
+endif
