84b7e492ce940f259f36cf482c051687dcd0f4fa
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-02-27 21:10:47 +0900

    サブディレクトリ内 Markdown のファイル間リンクを削除

---
+++ prod/doxygen-sample/src/cross-link/cross-link1.c
@@ -0,0 +1,12 @@
+/**
+ *  @brief          クロスリンクのサンプルファイル1。
+ *
+ *  クロスリンクのサンプルファイル 1 です。\n
+ *  Doxygen ではクロスリンク有効、Doxybook2 ではクロスリンク無効になります。
+ *
+ *  @see            cross_link2
+ */
+void cross_link1(void)
+{
+    return;
+}
---
+++ prod/doxygen-sample/src/cross-link/cross-link2.c
@@ -0,0 +1,12 @@
+/**
+ *  @brief          クロスリンクのサンプルファイル2。
+ *
+ *  クロスリンクのサンプルファイル 2 です。\n
+ *  Doxygen ではクロスリンク有効、Doxybook2 ではクロスリンク無効になります。
+ *
+ *  @see            cross_link1
+ */
+void cross_link2(void)
+{
+    return;
+}
