a61a8f5d05cae4c53a5afebb86e9fa0d6f321627
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-03-03 08:13:52 +0900

    サブモジュール修正

---
+++ prod/doxygen-sample/include/direction.h
@@ -0,0 +1,12 @@
+/**
+ *  @param[in]      a in パラメータ。
+ *                  - 引数 a に対する箇条書きのタイトル 1。
+ *                      - 引数 a に対する箇条書きのタイトル 1-1。
+ *  @param[out]     b out パラメータ。
+ *  @param[in,out]  c in,out パラメータ。
+ *  @param[inout]   d in,out (inout 表記) パラメータ。
+ *  @param          e 属性なしのパラメータ。
+ *                  - 引数 e に対する箇条書きのタイトル 1。
+ *                      - 引数 e に対する箇条書きのタイトル 1-1。
+ */
+void direction_func(int a, int *b, int *c, int *d, int *e);
