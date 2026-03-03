cd14071ef9be4061c9d3eb28ffdec869407d51f3
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-03-04 08:04:05 +0900

    サブモジュール更新

---
+++ prod/doxygen-sample/include/typedef_void.h
@@ -0,0 +1,8 @@
+/** @brief void ポインタ型。 */
+typedef void *TYPEDEF_VOID;
+
+/** @brief void ダブルポインタ型。 */
+typedef void **TYPEDEF_VOID_PP;
+
+/** @brief doxyfw において誤変換が発生しないかチェックするためのスニペット (誤変換事例があったため) */
+#define CHECK_STRING "\n*** CHECK STRING ***"
