4d98ebf9bdd9896d49a451d5f07b169a16237243
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-04-04 13:25:34 +0900

    feat: etw_providerTestとetw_sessionTestのmakepart.mkにコンパイルフラグを追加

--- test/util/src/libutilTest/etw_providerTest/makepart.mk
+++ test/util/src/libutilTest/etw_providerTest/makepart.mk
@@ -1,3 +1,6 @@
 # テスト対象のソースファイル
 TEST_SRCS := \
     $(WORKSPACE_FOLDER)/prod/util/libsrc/util/trace/etw-provider.c
+
+CFLAGS   += -I$(WORKSPACE_FOLDER)/prod/util/libsrc/util/trace
+CXXFLAGS += -I$(WORKSPACE_FOLDER)/prod/util/libsrc/util/trace
--- test/util/src/libutilTest/etw_sessionTest/makepart.mk
+++ test/util/src/libutilTest/etw_sessionTest/makepart.mk
@@ -2,3 +2,6 @@
 TEST_SRCS := \
     $(WORKSPACE_FOLDER)/prod/util/libsrc/util/trace/etw-provider.c \
     $(WORKSPACE_FOLDER)/prod/util/libsrc/util/trace/etw-session.c
+
+CFLAGS   += -I$(WORKSPACE_FOLDER)/prod/util/libsrc/util/trace
+CXXFLAGS += -I$(WORKSPACE_FOLDER)/prod/util/libsrc/util/trace
