77ca79ea54eaf72b3e9a4964e00799df21e85bee
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-04-09 21:21:13 +0900

    fix: makepart.mk のコメントを修正し、文法を統一

--- makepart.mk
+++ makepart.mk
@@ -25,13 +25,13 @@ endif
 
 # Windows EXE に activeCodePage=UTF-8 マニフェストを埋め込む
 # argv を含むプロセス全体の文字エンコードを UTF-8 に統一する（Win10 1903+）
-# この宣言によって、SetConsoleOutputCP(CP_UTF8); が不要となる。
+# この宣言によって、SetConsoleOutputCP(CP_UTF8) が不要となる。
 WIN32_MANIFEST = utf8
 
 # あわせて、コンソールでの UTF-8 文字化けを防ぐために、
 # #include <util/console/console.h> (LIBSDIR += $(WORKSPACE_FOLDER)/prod/util/lib) のうえで
-# console_init(); を組み込む必要がある。
-# console_dispose(); はライブラリアンロード時に自動的に呼ばれるため不要。
+# console_init() を組み込む必要がある。
+# console_dispose() はライブラリアンロード時に自動的に呼ばれるため不要。
 LIBSDIR += $(WORKSPACE_FOLDER)/prod/util/lib
 
 # console-util でマルチスレッドを利用するため、レポジトリ全体に -lpthread を指定しておく
