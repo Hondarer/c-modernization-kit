15b1d9be7b3d4c9c6e117261497b67f51e7aee4b
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-04-11 08:10:29 +0900

    feat: Start-VSCode-With-Env.cmd スクリプトを追加

---
+++ Start-VSCode-With-Env.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+REM Get current script directory
+set "SCRIPT_DIR=%~dp0"
+set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
+
+powershell.exe -ExecutionPolicy Bypass -File "%SCRIPT_DIR%\Start-VSCode-With-Env.ps1"
