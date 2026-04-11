7571718c5a7eb8edbbaf1ad1246fc086ac2fce8d
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-04-11 09:16:36 +0900

    fix: Start-VSCode-With-Env.cmd で引数を渡せるように修正

--- Start-VSCode-With-Env.cmd
+++ Start-VSCode-With-Env.cmd
@@ -4,4 +4,4 @@ REM Get current script directory
 set "SCRIPT_DIR=%~dp0"
 set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
 
-powershell.exe -ExecutionPolicy Bypass -File "%SCRIPT_DIR%\bin\Start-VSCode-With-Env.ps1"
+powershell.exe -ExecutionPolicy Bypass -File "%SCRIPT_DIR%\bin\Start-VSCode-With-Env.ps1" %*
