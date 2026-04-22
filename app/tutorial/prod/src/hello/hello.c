ccf349951d865a9a00f212f3dc97d1a4da512c74
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-04-23 08:32:40 +0900

    各サブプロジェクトのコミットを最新のものに更新し、hello.c サンプルアプリケーションを追加

---
+++ app/tutorial/prod/src/hello/hello.c
@@ -0,0 +1,36 @@
+/**
+ *******************************************************************************
+ *  @file           hello.c
+ *  @brief          サンプルアプリケーション。
+ *  @author         Tetsuo Honda
+ *  @date           2026/04/23
+ *  @version        1.0.0
+ *
+ *  @copyright      Copyright (C) Tetsuo Honda. 2026. All rights reserved.
+ *
+ *******************************************************************************
+ */
+
+#include <com_util/console/console.h>
+#include <stdio.h>
+
+/**
+ *  @brief          アプリケーションのエントリポイント。
+ *
+ *  @param[in]      argc コマンドライン引数の個数。
+ *  @param[in]      argv コマンドライン引数の文字列の配列。
+ *  @return         常に 0 を返します。
+ */
+int main(int argc, char *argv[])
+{
+    console_init();
+
+    printf("✨ Hello, c-modernization-kit! ✨\n");
+
+    if (argc >= 2)
+    {
+        printf("echo: %s\n", argv[1]);
+    }
+
+    return 0;
+}
---
+++ app/tutorial/prod/src/makepart.mk
@@ -0,0 +1,5 @@
+# 出力ディレクトリ
+OUTPUT_DIR := $(MYAPP_DIR)/prod/bin
+
+# ライブラリの指定
+LIBS += com_util
