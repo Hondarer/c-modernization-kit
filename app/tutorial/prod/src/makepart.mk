b9d64a96962bcc0f6c08ec7ae22ccfab6b130e50
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-05-07 07:36:42 +0900

    link 時に .exp と .lib を生成しないようにする。

--- app/tutorial/prod/src/makepart.mk
+++ app/tutorial/prod/src/makepart.mk
@@ -7,4 +7,7 @@ LIBS += com_util_static
 ifdef PLATFORM_WINDOWS
     CFLAGS   += /DCOM_UTIL_STATIC
     CXXFLAGS += /DCOM_UTIL_STATIC
+    # libcom_util は both 生成のため、static library にも export テーブルを持っている。
+    # link 時に .exp と .lib を生成しないようにする。
+    LDFLAGS  += /NOEXP /NOIMPLIB
 endif
