4887dd3d8da29eb68ec0c77c6221d45a356e8481
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-06-01 20:43:26 +0900

    [inout]を非推奨とする

--- app/doxygen-sample/prod/include/direction.h
+++ app/doxygen-sample/prod/include/direction.h
@@ -4,7 +4,7 @@
  *                      - 引数 a に対する箇条書きのタイトル 1-1。
  *  @param[out]     b out パラメータ。
  *  @param[in,out]  c in,out パラメータ。
- *  @param[inout]   d in,out (inout 表記) パラメータ。
+ *  @param[inout]   d in,out (inout 表記) パラメータ。非推奨です。
  *  @param          e 属性なしのパラメータ。
  *                  - 引数 e に対する箇条書きのタイトル 1。
  *                      - 引数 e に対する箇条書きのタイトル 1-1。
